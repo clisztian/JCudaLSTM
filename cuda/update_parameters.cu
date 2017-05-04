73a6ebcd439ba6e58e1547b95b26e1d5b43e5833
clisztian <clisztian@gmail.com>
2017-05-04 15:44:39 +0200

    fixed update_parameters.cu

--- cuda/update_parameters.cu
+++ cuda/update_parameters.cu
@@ -9,7 +9,7 @@
 
 #include <math.h> 
 
-extern "C"
+extern C
 __global__ void update_parameters(int n, double stepsize, double decayRate, double reg, double smoothEpsilon, 
                         double gradientClip, double *w, double *dw, double *cached)
 {
@@ -36,7 +36,7 @@ __global__ void update_parameters(int n, double stepsize, double decayRate, doub
      }
 }
 
-extern "C"
+extern C
 __global__ void reset_zero(int n, double *w, double *dw, double *cached)
 {
  
@@ -50,7 +50,21 @@ __global__ void reset_zero(int n, double *w, double *dw, double *cached)
      }
 }
 
-extern "C"
+extern C
+__global__ void resetdw_zero(int n, double *dw, double *cached)
+{
+ 
+	 int i = blockIdx.x * blockDim.x + threadIdx.x;
+	 
+	 if (i<n)
+	 {
+		dw[i] = 0;
+		cached[i] = 0;
+     }
+}
+
+
+extern C
 __global__ void reset_zero_all(int n, double *w, double *dw, double *cached,
                                   double *wa, double *dwa, double *cacheda,
                                   double *wb, double *dwb, double *cachedb)
@@ -73,7 +87,7 @@ __global__ void reset_zero_all(int n, double *w, double *dw, double *cached,
 }
 
 
-extern "C"
+extern C
 __global__ void reset_zero_lstm(int n, double  *w, double  *dw, double  *cached,
                                        double *wa, double *dwa, double *cacheda,
                                        double *wb, double *dwb, double *cachedb,
@@ -107,5 +121,305 @@ __global__ void reset_zero_lstm(int n, double  *w, double  *dw, double  *cached,
 		cachedd[i] = 0;
      }
 }
+
+
+extern C
+__global__ void reset_zero_rwa(int n, double  *w, double  *dw, double  *cached,
+                                       double *wa, double *dwa, double *cacheda)
+                                   
+{
+ 
+	 int i = blockIdx.x * blockDim.x + threadIdx.x;
+	 
+	 if (i<n)
+	 {
+	    w[i] = 0;
+		dw[i] = 0;
+		cached[i] = 0;
+		
+		wa[i] = 0;
+		dwa[i] = 0;
+		cacheda[i] = 0;
+     }
+}
+
+
+extern C
+__global__ void normalize(double *g_idata, double *g_odata, unsigned int n, int maxIndx)
+{
+ 
+    double max = g_idata[maxIndx];      
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    if(i < n)
+    {
+        g_odata[i] = exp(g_idata[i] - max);
+    }
+
+}
+
+extern C
+__global__ void getTargetIndex(int n, int *index, double *w)
+{
+ 
+   int i = blockIdx.x * blockDim.x + threadIdx.x;
+   if (i<n)
+   {
+     if(w[i] == 1.0) {index[0] = i;}
+   }
+}
+
+
+extern C
+__global__ void mismatch(int n, double* actual, double *target, int *mis)
+{
+ 
+   mis[0] = 0;
+   
+   int i = blockIdx.x * blockDim.x + threadIdx.x;
+   if (i<n)
+   {
+     if(target[i] >= 0.5 && actual[i] < 0.5) {mis[0] = 1;}
+     if(target[i] < 0.5 && actual[i] >= 0.5) {mis[0] = 1;}
+   }
+}
+
+
+extern C
+__global__ void setTargetIndex(int n, double *w, double *out)
+{
+ 
+   int i = blockIdx.x * blockDim.x + threadIdx.x;
+   if (i<n)
+   {
+     if(w[i] == 1.0) {out[i] -= 1.0;}
+   }
+}
+
+extern C
+__global__ void setTargetIndexNormalize(int n, double sum, double *w, double *out, double* output)
+{
+ 
+   int i = blockIdx.x * blockDim.x + threadIdx.x;
+   if (i<n)
+   {
+     if(w[i] == 1.0) {output[0] = out[i]/sum;}
+   }
+}
+
+extern C
+__global__ void backwardError(int n, double *actual, double *target, double* out)
+{
+ 
+   int i = blockIdx.x * blockDim.x + threadIdx.x;
+   if (i<n)
+   {
+     out[i] += (actual[i] - target[i]);
+   }
+}
+
+extern C
+__global__ void difference(int n, double *actual, double *target, double* out)
+{
+ 
+   int i = blockIdx.x * blockDim.x + threadIdx.x;
+   if (i<n)
+   {
+     out[i] = (actual[i] - target[i]);
+   }
+}
+
+
+	        
+extern C
+__global__ void multiadd(int n, double *dw, double *temp, double *outdw)
+{
+	int i = blockIdx.x * blockDim.x + threadIdx.x;
+	if (i<n)
+	            {
+	                dw[i] = dw[i] + temp[i]*outdw[i];
+	            }
+	        } 
+	        
+			extern C
+	        __global__ void concat(int n, int shift, double *ow, double *odw, double *ostepcache, double *w, double *dw, double *stepcache)
+	        {
+	            int i = blockIdx.x * blockDim.x + threadIdx.x;
+	            if (i<n)
+	            {
+	                ow[shift + i] = w[i];
+	                odw[shift + i] = dw[i];
+	                ostepcache[shift + i] = stepcache[i];
+	            }
+	        } 
+	        
+			extern C
+			__global__ void concatback(int n, int shift, double *ow, double *odw, double *ostepcache, double *w, double *dw, double *stepcache)
+			{
+			    int i = blockIdx.x * blockDim.x + threadIdx.x;
+			    if (i<n)
+			    {
+			        w[i] = ow[shift + i];
+			        dw[i] = odw[shift + i];
+			        stepcache[i] = ostepcache[shift + i];
+			    }
+			} 
+	        
+	        extern C
+	        __global__ void add(int n, double *m1, double *m2, double *outdw)
+	        {
+	            int i = blockIdx.x * blockDim.x + threadIdx.x;
+	            if (i<n)
+	            {
+	                outdw[i] = m1[i] + m2[i];
+	            }
+	        } 
+	        extern C
+	        __global__ void addbatch(int nrows, int nbatch, double *m1, double *m2, double *outdw)
+	        {
+	            int i = blockIdx.x*blockDim.x + threadIdx.x;
+	            int j = blockIdx.y*blockDim.y + threadIdx.y;
+	            if (i<nrows)
+	            {
+	              if(j < nbatch)
+	              {
+	                outdw[i*nbatch + j] = m1[i*nbatch + j] + m2[i];
+	              }
+	            }
+	        } 
+	        extern C
+	        __global__ void addbatchback(int nrows, int nbatch, double *m1, double *m2, double *outdw)
+	        {
+	            int i = blockIdx.x*blockDim.x + threadIdx.x;
+	            int j = blockIdx.y*blockDim.y + threadIdx.y;
+	            if (i < nrows)
+	            {
+	              if(j < nbatch)
+	              {
+	                m1[i*nbatch + j] = m1[i*nbatch + j] + outdw[i*nbatch + j];
+	              }
+	            }
+	        }
+	        extern C
+	        __global__ void addbatchbackrow(int nrows, int nbatch, int col, double *m1, double *m2, double *outdw)
+	        {
+	            int i = blockIdx.x*blockDim.x + threadIdx.x;
+	            if (i < nrows)
+	            {
+	                m2[i] +=  outdw[i*nbatch + col]/nbatch; 
+	            }
+	        }   
+	        
+	        
+	        
+	        extern C
+	        __global__ void sub(int n, double *m1, double *m2, double *outdw)
+	        {
+	            int i = blockIdx.x * blockDim.x + threadIdx.x;
+	            if (i<n)
+	            {
+	                outdw[i] = m1[i] - m2[i];
+	            }
+	        } 
+	        extern C
+	        __global__ void subback(int n, double *m1dw, double *m2dw, double *outdw)
+	        {
+	            int i = blockIdx.x * blockDim.x + threadIdx.x;
+	            if (i<n)
+	            {
+	                m1dw[i] = m1dw[i] + outdw[i];
+	                m2dw[i] = m2dw[i] - outdw[i];
+	            }
+	        } 
+	        
+	        
+	        
+	        
+	        extern C
+	        __global__ void elemult(int n, double *a, double *b, double *out)
+	        {
+	            int i = blockIdx.x * blockDim.x + threadIdx.x;
+	            if (i<n)
+	            {
+	                out[i] = a[i]*b[i];
+	            }
+	        } 	        
+	        extern C
+	        __global__ void crossmult(int n, double *m1dw, double *m2dw, double *m1w, double *m2w, double *outdw)
+	        {
+	            int i = blockIdx.x * blockDim.x + threadIdx.x;
+	            if (i<n)
+	            {
+	                m1dw[i] = m1dw[i] + m2w[i] * outdw[i];
+	                m2dw[i] = m2dw[i] + m1w[i] * outdw[i];
+	            }
+	        }
+	        
+	        extern C
+	        __global__ void elediv(int n, double *a, double *b, double *out)
+	        {
+	            int i = blockIdx.x * blockDim.x + threadIdx.x;
+	            if (i<n)
+	            {
+	                out[i] = a[i]/b[i];
+	            }
+	        } 
+	        
+	        extern C
+	        __global__ void crossdiv(int n, double *m1dw, double *m2dw, double *m1w, double *m2w, double *outdw)
+	        {
+	            int i = blockIdx.x * blockDim.x + threadIdx.x;
+	            if (i<n)
+	            {
+	                m1dw[i] = m1dw[i] + outdw[i]/m2w[i];
+	                m2dw[i] = m2dw[i] - m1w[i] * outdw[i]/(m2w[i] * m2w[i]);
+	            }
+	        }
+	        
+	        extern C
+	        __global__ void mmKernel(int m, int k, int n, double *m1dw, double *m2dw, double *m1w, double *m2w, double *outdw) {
+	           int i = blockIdx.x*blockDim.x+threadIdx.x;
+	           int j = blockIdx.y*blockDim.y+threadIdx.y;
+	           double b = 0;
+	           if (i < m && j < k) {
+	             b = outdw[i*n + j];
+	             for (int l = 0; l < n; l++) {
+	        		m1dw[k*i + l] += m2w[l*n+ j]*b;
+	        		m2dw[n*l + j] += m1w[i*k + l]*b;
+	        	  }
+	           }
+	        } 
+	        extern C
+	        __global__ void maximum(int n, double *m1, double *m2, double *m1cache, double *out) {
+	           int i = blockIdx.x*blockDim.x+threadIdx.x;
+	           if (i<n)
+	           {
+	             if(m1[i] > m2[i]) 
+	             {  + \n +
+	                out[i] = m1[i];
+	                m1cache[i] = 1;
+	             }
+	             else {  + \n +
+	                out[i] = m2[i];  + \n +
+	                m1cache[i] = 0;
+	             }
+	           }
+	        } 
+	        extern C
+	        __global__ void maximumback(int n, double *m1dw, double *m2dw, double *m1cache, double *out) {
+	           int i = blockIdx.x*blockDim.x+threadIdx.x;
+	           if (i<n)
+	           {
+	             if(m1cache[i] > 0) 
+	             {  + \n +
+	                m1dw[i] += out[i];
+	             }
+	             else {  + \n +
+	                m2dw[i] += out[i];  + \n +
+	             }
+	           }
+	        }
+
+
+
 
 
